ca78141a22bfc353a3902bf2b08df3f1d84290b8
Maya Liu <maya.liu@acquia.com>
2015-12-17 17:51:29 -0500

    Day 17 stubs

---
+++ src/com/mayaliu/codeadvent/day17/Eggnog.java
@@ -0,0 +1,27 @@
+package com.mayaliu.codeadvent.day17;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.mayaliu.codeadvent.common.CommonUtils;
+
+/**
+ * @author MayaYL
+ * 
+ * Solution for adventofcode's Day 17 puzzle, December 2015. See 
+ * http://adventofcode.com/day/17
+ */
+public class Eggnog {
+	
+	/**
+	 * @param args
+	 *   The string or file path containing the list of Aunt Sues.
+	 *   
+	 * @throws Exception 
+	 * @throws NumberFormatException 
+	 */
+	public static void main(String[] args) throws NumberFormatException, Exception {
+		String containerData = CommonUtils.processInput(args, "EggNog"");
+	}
+}
