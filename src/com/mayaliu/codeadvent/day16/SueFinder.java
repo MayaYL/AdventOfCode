af33921c4ef8fb133dc0011239e601b21b494f5a
Maya Liu <yeliu2428@gmail.com>
2015-12-20 21:33:35 -0500

    Finishing up day 16.

--- src/com/mayaliu/codeadvent/day16/SueFinder.java
+++ src/com/mayaliu/codeadvent/day16/SueFinder.java
@@ -30,20 +30,19 @@ public class SueFinder {
 		expectedValues.put("cats", 7);
 		expectedValues.put("samoyeds", 2);
 		expectedValues.put("pomeranians", 3);
-		expectedValues.put("akitas", null);
-		expectedValues.put("vizslas", null);
+		expectedValues.put("akitas", 0);
+		expectedValues.put("vizslas", 0);
 		expectedValues.put("goldfish", 5);
 		expectedValues.put("trees", 3);
 		expectedValues.put("cars", 2);
 		expectedValues.put("perfumes", 1);
-		ArrayList<Sue> possibles = new ArrayList<Sue>();
 		
 		for (String sue : sueList) {
 			// Find the ID. The very first number in the line will be the ID.
 			Pattern idPattern = Pattern.compile("[0-9]+");
 			Matcher idMatcher = idPattern.matcher(sue);
 			idMatcher.find();
-			Sue s = new Sue(Integer.parseInt(idMatcher.group()));
+			int sueNumber = Integer.parseInt(idMatcher.group());
 			
 			// Find each of the possible properties.
 			// Pattern: "goldfish: 9," which is (string): (int)
@@ -55,169 +54,37 @@ public class SueFinder {
 				String[] dataParts = data.split(": ");
 				String propertyName = dataParts[0];
 				int propertyValue = Integer.parseInt(dataParts[1]);
-				if (expectedValues.get(propertyName) == null) {
-					if (propertyValue != 0) {
-						continue;
+				int expectedValue = Integer.valueOf(expectedValues.get(propertyName));
+				
+				// Check for the ranged values.
+				if (propertyName.equals("cats") || propertyName.equals("trees")) {
+					if (propertyValue <= expectedValue) {
+						matched = false;
+						break;
 					}
 				}
-				else if (Integer.valueOf(expectedValues.get(propertyName)) != propertyValue) {
-					continue;
+				else if (propertyName.equals("pomeranians") || propertyName.equals("goldfish")) {
+					if (propertyValue >= expectedValue) {
+						matched = false;
+						break;
+					}
 				}
-//				if (!(expectedValues.get(propertyName) == null && propertyValue == 0)
-//						&& !(Integer.valueOf(expectedValues.get(propertyName)) == (propertyValue))) {
-//					continue;
-//				}
-				matched = true;
-				s.add(propertyName, propertyValue);
-			}
-			if (matched) {
-				possibles.add(s);
-			}
-			
-//			System.out.format("Sue number %d sent the package.\n", s.getId());
-			
-			
-//			System.out.println(s.getProperties().keySet());
-//			for (String str : s.getProperties().keySet()) {
-//
-//				System.out.print(s.getProperties().get(str)+", ");
-//			}
-//			System.out.print(s.getProperties().get("pomeranians"));
-//			System.out.println();
-		}
-		for (String property : expectedValues.keySet()) {
-			for (Sue s : possibles) {
-				HashMap<String, Integer> sueProperties = s.getProperties();
-				if (sueProperties.containsKey(property)) {
-					if (sueProperties.get(property) == expectedValues.get(property)) {
-						
+				// Not a ranged value.
+				else {
+					if (expectedValue != propertyValue) {
+						matched = false;
+						break;
 					}
 					else {
-						continue;
+						matched = true;
 					}
 				}
-				else {
-					continue;
-				}
+			}
+			
+			if (matched) {
+				System.out.format("Sue number %d sent the package.\n", sueNumber);
+				break;
 			}
 		}
-	}
-
-	/**
-	 * A Sue object containing various information about the specific Sue.
-	 * 
-	 * @author MayaYL
-	 */
-	private static class Sue {
-		private int id, children, cats, samoyeds, pomeranians, akitas, vizslas;
-		private int goldfish, trees, cars, perfumes;
-		private HashMap<String, Integer> properties;
-		
-		public Sue(int id) {
-			this.id = id;
-			this.properties = new HashMap<String, Integer>();
-		}
-		
-		public void add(String property, int number) {
-			this.properties.put(property, new Integer(number));
-		}
-		
-		public HashMap<String, Integer> getProperties() {
-			return this.properties;
-		}
-
-		public int getId() {
-			return id;
-		}
-//
-//		public void setId(int id) {
-//			this.id = id;
-//		}
-//
-//		public int getChildren() {
-//			return children;
-//		}
-//
-//		public void setChildren(int children) {
-//			this.children = children;
-//		}
-//
-//		public int getCats() {
-//			return cats;
-//		}
-//
-//		public void setCats(int cats) {
-//			this.cats = cats;
-//		}
-//
-//		public int getSamoyeds() {
-//			return samoyeds;
-//		}
-//
-//		public void setSamoyeds(int samoyeds) {
-//			this.samoyeds = samoyeds;
-//		}
-//
-//		public int getPomeranians() {
-//			return pomeranians;
-//		}
-//
-//		public void setPomeranians(int pomeranians) {
-//			this.pomeranians = pomeranians;
-//		}
-//
-//		public int getAkitas() {
-//			return akitas;
-//		}
-//
-//		public void setAkitas(int akitas) {
-//			this.akitas = akitas;
-//		}
-//
-//		public int getVizslas() {
-//			return vizslas;
-//		}
-//
-//		public void setVizslas(int vizslas) {
-//			this.vizslas = vizslas;
-//		}
-//
-//		public int getGoldfish() {
-//			return goldfish;
-//		}
-//
-//		public void setGoldfish(int goldfish) {
-//			this.goldfish = goldfish;
-//		}
-//
-//		public int getTrees() {
-//			return trees;
-//		}
-//
-//		public void setTrees(int trees) {
-//			this.trees = trees;
-//		}
-//
-//		public int getCars() {
-//			return cars;
-//		}
-//
-//		public void setCars(int cars) {
-//			this.cars = cars;
-//		}
-//
-//		public int getPerfumes() {
-//			return perfumes;
-//		}
-//
-//		public void setPerfumes(int perfumes) {
-//			this.perfumes = perfumes;
-//		}
-		
-//		int id, int children, int
-//		cats, int samoyeds, int pomeranians, int akitas, int vizslas,
-//		int goldfish, int
-//		trees, int
-//		cars, int perfumes
 	}
 }
